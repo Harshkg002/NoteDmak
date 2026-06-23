0cef3693faa04ac47c3d397440fc50fffad67ab7
Harshkg002 <harshkgupta002@gmail.com>
2026-06-23 23:58:03 +0530

    handled ui for when you have zero notes

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -36,6 +36,7 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUI/>}
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {isLoading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
+        {!isLoading && notes.length ===0 && <div className='text-center text-primary py-10'>Add a new note by clicking create Note!!</div>}
         {notes.length >0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map(note=>(
